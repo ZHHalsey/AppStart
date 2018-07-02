38db4e420854fe3485cacce78144b0f6f83fa256
zhanghao <739415161@qq.com>
2018-07-02 16:12:52 +0800

    把zip换成可编辑代码

---
+++ 网络视频的加载/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  网络视频的加载
+//
+//  Created by 张豪 on 2018/4/25.
+//  Copyright © 2018年 张豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
